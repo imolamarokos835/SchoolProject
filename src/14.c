439a851bd298b8b55446f52ec6c49785e065cb41
imolamarokos835 <imolamarokos835@gmail.com>
2025-03-19 21:05:47 +0000

    Add 14.c

---
+++ src/14.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int age = 15;
+  if (age > 18) {
+    printf("You are old enough to drink.\n");
+  } else {
+    printf("You are not old enough to drink.\n");
+  }
+  return 0;
+}
