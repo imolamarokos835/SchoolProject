520e9fe145912b3acb3a5bf6826084b29aa43a6a
imolamarokos835 <imolamarokos835@gmail.com>
2025-04-11 10:46:56 +0000

    Add 29.c

---
+++ src/29.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    if (n % 2 == 0) {
+        printf("The number is even.\n");
+    } else {
+        printf("The number is odd.\n");
+    }
+}
