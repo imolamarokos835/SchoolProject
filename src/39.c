ad0e060e7fb06a6876774ea5543a00a28515d283
imolamarokos835 <imolamarokos835@gmail.com>
2025-05-20 11:18:43 +0000

    Add 39.c

---
+++ src/39.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int num1 = 5;
+    int num2 = 3;
+
+    if (num1 > num2) {
+        printf("Num 1 is greater than Num 2\n");
+    } else if (num1 == num2) {
+        printf("Num 1 is equal to Num 2\n");
+    } else {
+        printf("Num 1 is less than Num 2\n");
+    }
+
+    return 0;
+}
